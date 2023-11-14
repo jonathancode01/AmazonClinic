f6e60770f594d9a9dd7e6d9b73eb882f82973c84
jonathancode01 <icontatojonathan@gmail.com>
2023-11-14 20:56:53 -0300

    Nova Atualização 14-11

---
+++ JavaScript/Tmedico.js
@@ -0,0 +1,12 @@
+
+
+function abrirAlerta(){
+    Swal.fire({
+    title: "Sweet!",
+    text: "Modal with a custom image.",
+    imageUrl: "https://imgv2-2-f.scribdassets.com/img/document/626026916/original/37620e4959/1697138018?v=1",
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: "Custom image"
+    });
+}
