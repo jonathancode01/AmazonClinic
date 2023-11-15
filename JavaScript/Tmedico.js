adb010bf667892b8744a4c7498fe94941451dc42
jonathancode01 <icontatojonathan@gmail.com>
2023-11-15 01:23:09 -0300

    Atualização 15-11

--- JavaScript/Tmedico.js
+++ JavaScript/Tmedico.js
@@ -1,12 +1,17 @@
+const formEl = document.getElementById('form-api')
+
+    formEl.addEventListener('submit', evento => {
+        evento.preventDefault();
+
+        const formData = new FormData(formEl);
+        const data = Object.fromEntries(formData);
 
 
-function abrirAlerta(){
-    Swal.fire({
-    title: "Sweet!",
-    text: "Modal with a custom image.",
-    imageUrl: "https://imgv2-2-f.scribdassets.com/img/document/626026916/original/37620e4959/1697138018?v=1",
-    imageWidth: 400,
-    imageHeight: 200,
-    imageAlt: "Custom image"
-    });
-}
+        fetch('https://reqres.in/api/users',{
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(res => res.json()).then(data => console.log(data))
+    })
